4c5e914d02d5e0bb85f8d5470783d8e27b35cf96
vkRajan18 <vkrajan18122003@gmail.com>
2026-06-20 19:13:07 +0530

    initial setup

--- index.js
+++
@@ -1,9 +0,0 @@
-
-
-const parent = React.createElement("div", {id:"parent"},[
-    React.createElement("div",{},React.createElement("h1", {},"im h1 tag"))
-])
-
-const heading = React.createElement("h1", {id:"hello"}, "hello from react");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
